e02900a8f0df05bede75c0c672a8f22710108fd7
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-03-18 10:40:43 +0900

    ESLintの警告が出ないよう修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,7 @@ module.exports = {
       env: {
         node: true
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
         sourceType: 'script'
       }
@@ -22,6 +20,5 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  rules: {
-  }
+  rules: {}
 }

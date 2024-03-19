c6112a38ada81aaf50e3e43e7b1b9139d19a5e59
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-03-19 14:25:31 +0900

    ログアウトのリンクを追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,7 +10,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user#, event: :authentication # this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
+      session["devise.google_oauth2_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
       redirect_to new_user_registration_url
     end
   end

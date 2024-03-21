a818f0abc97d50550ff3e4aa5e42d30cae7d70f0
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-03-21 21:57:59 +0900

    不要なファイルを削除

--- test/controllers/statics_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class StaticsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/users/omniauth_callbacks_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Users::OmniauthCallbacksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end

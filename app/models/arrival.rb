4c7be86e71b6974403fe21980bb22fcf134bfccb
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-03-25 09:40:39 +0900

    ArrrivalをWalkに対して複数追加できるよう制約を修正

--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -1,4 +1,5 @@
 class Arrival < ApplicationRecord
   belongs_to :walk
   belongs_to :station
+  validates :station_id, presence: true, uniqueness: {scope: :walk_id}
 end
--- db/migrate/20240323055616_create_arrivals.rb
+++ db/migrate/20240323055616_create_arrivals.rb
@@ -1,12 +1,13 @@
 class CreateArrivals < ActiveRecord::Migration[7.1]
   def change
     create_table :arrivals do |t|
-      t.belongs_to :walk, index: { unique: true }, foreign_key: true
-      t.belongs_to :station, index: { unique: true }, foreign_key: true
+      t.belongs_to :walk, foreign_key: true
+      t.belongs_to :station, foreign_key: true
       t.string :memo
       t.date :arrived_at
 
       t.timestamps
     end
+    add_index :arrivals, [:walk_id, :station_id], unique: true
   end
 end

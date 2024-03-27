f95525435ad329e0c29329e541aeeedd8f30c252
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-03-27 11:02:29 +0900

    ログイン後の画面遷移を修正

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,7 +1,3 @@
 class StaticsController < ApplicationController
-  def top
-    return unless user_signed_in?
-
-    redirect_to walk_url
-  end
+  def top; end
 end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,8 +6,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_or_create_by(provider: auth.provider, uid: auth.uid)
 
     if @user.persisted?
-      sign_in_and_redirect @user
+      sign_in @user
       set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+      redirect_to @user.walk ? walk_path : new_walk_path
     else
       session['devise.google_oauth2_data'] = request.env['omniauth.auth'].except(:extra)
       redirect_to new_user_registration_url

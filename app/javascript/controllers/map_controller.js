fb30b090a7048e1b299cb4900f96b77ec9f63cdf
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-03-28 15:32:33 +0900

    初期設定のピンとラインを追加

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,18 +1,43 @@
 import { Controller } from "@hotwired/stimulus";
+import { allStations } from "./stations";
 
 // Connects to data-controller="map"
 export default class extends Controller {
-  connect() {
-    /* eslint-disable no-undef */
-    const map = L.map("map").setView([35.6583345416667, 139.699565752778], 13);
+  static targets = ["ids"];
 
+  /* eslint-disable no-undef */
+  initialize() {
+    this.map = L.map("map").setView([35.6583345416667, 139.699565752778], 13);
+  }
+
+  connect() {
     L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>Tiles Ⓒ <a href="">HOT</a>',
-    }).addTo(map);
+    }).addTo(this.map);
+    this.addPin(allStations);
+    this.addLine(allStations);
   }
 
-  greet() {
-    console.log("hello");
+  addPin(stations) {
+    for (let i = 0; i < stations.length; i++) {
+      const station = stations[i];
+      L.marker([station.latitude, station.longitude]).addTo(this.map).bindPopup(station.name);
+    }
+  }
+
+  addLine(stations) {
+    const locations = [];
+    for (let i = 0; i < stations.length; i++) {
+      const station = stations[i];
+      locations.push([station.latitude, station.longitude]);
+    }
+    L.polyline(locations, { color: "green", weight: 10, opacity: 0.5 }).addTo(this.map);
+    console.log(locations);
+  }
+
+  draw() {
+    this.addPin();
+    // const ids = this.idsTarget.value;
   }
 }

75885f719396706002a4279800dd23911e813ea2
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-03-29 21:39:18 +0900

    ロケールファイルを作成

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,7 @@ module Yamanotes
     #
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end

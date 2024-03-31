4a1390d816985511243cea800fb1a5756ad00f20
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-03-31 16:23:42 +0900

    Userにバリデーションと制約を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  devise :omniauthable, omniauth_providers: %i[google_oauth2]
   has_one :walk, dependent: :destroy
   has_many :arrivals, through: :walk
-  devise :omniauthable, omniauth_providers: %i[google_oauth2]
+  validates :uid, uniqueness: { scope: :provider }, numericality: { only_integer: true }, presence: true
+  validates :provider, inclusion: { in: ["google_oauth2"] }, presence: true
 end

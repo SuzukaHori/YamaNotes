ffa92c81c3c494aec5a4a2d91b99005720f620ba
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-04-01 14:55:36 +0900

    launchyを導入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,4 +62,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:each, type: :system) do
+    driven_by :selenium_chrome
+  end
+
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end

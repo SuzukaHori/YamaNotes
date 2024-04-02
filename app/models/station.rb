9572bf43c8f2846b2f37f1bd400012c1792b2182
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-04-02 17:28:24 +0900

    残り駅数・距離の追加

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,8 @@
 class Station < ApplicationRecord
   has_many :arrivals, dependent: :destroy
   has_many :walks, through: :arrivals
+
+  def self.total_distance
+    Station.all.sum(&:clockwise_distance_to_next)
+  end
 end

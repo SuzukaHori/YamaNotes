8b95a69e9e611f789a04800ec734200d142ce94e
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-04-02 18:06:17 +0900

    最終駅で地図の直線が途切れるバグを修正

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -25,17 +25,14 @@ export default class extends Controller {
     const map = L.map("map").setView(centerPosition, 12);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 19,
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright/ja">OpenStreetMap</a>contributors',
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright/ja">OpenStreetMap</a>contributors',
     }).addTo(map);
     return map;
   }
 
   setArrivedStations() {
     const throughStationIds = JSON.parse(this.throughStationIdsTarget.value);
-    return throughStationIds.map((id) =>
-      this.allStations.find((station) => station.id === id),
-    );
+    return throughStationIds.map((id) => this.allStations.find((station) => station.id === id));
   }
 
   addPins(stations) {
@@ -45,14 +42,9 @@ export default class extends Controller {
     for (let i = 0; i < stations.length; i++) {
       const station = stations[i];
       if (station.id === this.currentStationId) {
-        L.marker([station.latitude, station.longitude], { icon: myIcon })
-          .addTo(this.map)
-          .bindPopup(`${station.name}駅`)
-          .openPopup();
+        L.marker([station.latitude, station.longitude], { icon: myIcon }).addTo(this.map).bindPopup(`${station.name}駅`).openPopup();
       } else {
-        L.marker([station.latitude, station.longitude], { icon: myIcon })
-          .addTo(this.map)
-          .bindPopup(`${station.name}駅`);
+        L.marker([station.latitude, station.longitude], { icon: myIcon }).addTo(this.map).bindPopup(`${station.name}駅`);
       }
     }
   }
@@ -62,6 +54,9 @@ export default class extends Controller {
     for (let i = 0; i < stations.length; i++) {
       const station = stations[i];
       locations.push([station.latitude, station.longitude]);
+    }
+    if (stations.length === this.allStations.length) {
+      locations.push([stations[0].latitude, stations[0].longitude]);
     }
     L.polyline(locations, { color, weight: 15, opacity: 0.4 }).addTo(this.map);
   }

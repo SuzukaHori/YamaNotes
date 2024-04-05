c0fdde58d5e3cc6ff1f965694ee23e7c63623521
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-04-05 21:56:20 +0900

    Stationテーブルから不要なカラムを削除し、次の駅idを追加

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,6 +1,8 @@
 class Station < ApplicationRecord
   has_many :arrivals, dependent: :destroy
   has_many :walks, through: :arrivals
+  has_one :station, class_name: 'Station', inverse_of: :clockwise_next_station, dependent: :destroy
+  belongs_to :clockwise_next_station, class_name: 'Station', inverse_of: :station
 
   def next
     next_id = id == Station.all.map(&:id).max ? 1 : id + 1
--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_23_055616) do
     t.float "longitude", null: false
     t.float "latitude", null: false
     t.float "clockwise_distance_to_next", null: false
-    t.float "counterclockwise_distance_to_next", null: false
+    t.integer "clockwise_next_station_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

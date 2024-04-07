ae30531474071b62718d8547bd6d9a6683995e60
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-04-07 19:06:16 +0900

    隣駅以外に到着でいないようバリデーションを設定

--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -2,4 +2,19 @@ class Arrival < ApplicationRecord
   belongs_to :walk
   belongs_to :station
   validates :arrived_at, presence: true
+  validate :prohibit_arrival_without_next_station
+
+  def prohibit_arrival_without_next_station
+    return if walk.arrivals.length == 1
+
+    is_correct_next_station =
+      if walk.clockwise
+        walk.current_station.clockwise_next_station == station
+      else
+        station == Station.find_by(clockwise_next_station: walk.current_station)
+      end
+    return if is_correct_next_station
+
+    errors.add :station_id, '隣駅以外に到着することはできません'
+  end
 end

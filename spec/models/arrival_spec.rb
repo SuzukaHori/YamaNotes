85e5c4a6e36b67979cea4141bde2878d09a67c19
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-04-08 15:11:12 +0900

    内回りモードの最低限のテストを作成

--- spec/models/arrival_spec.rb
+++ spec/models/arrival_spec.rb
@@ -14,17 +14,9 @@ RSpec.describe Arrival, type: :model do
     end
 
     it '近接しない駅には到着できない' do
-      # TODO: add validation
-      # expect {
-      #   @walk.reload.arrivals.create!(station_id: 13, arrived_at: Time.current)
-      # }.to change { Arrival.count }.by(1)
-    end
-
-    it '一周終了時に最初の駅の到着時刻を更新する' do
-      (2..30).each do |n|
-        @walk.arrivals.create!(station_id: n, arrived_at: Time.current)
-      end
-      @arrival.update!(station_id: 1, arrived_at: Time.current)
+      expect {
+        @walk.arrivals.create!(station_id: 13, arrived_at: Time.current)
+      }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 end
--- spec/models/walk_spec.rb
+++ spec/models/walk_spec.rb
@@ -1,39 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe Walk, type: :model do
-  before do
-    @walk = FactoryBot.create(:walk)
-    @arrival = @walk.arrivals.create!(station_id: 1, arrived_at: Time.current)
-  end
-
   describe '現在の駅を取得する' do
-    it '到着済みの駅がない時、出発駅が現在の駅になる' do
-      expect(@walk.current_station.name).to eq '品川'
+    context '外回りモードの時' do
+      before do
+        @walk = FactoryBot.create(:walk)
+        @arrival = @walk.arrivals.create!(station_id: 1, arrived_at: Time.current)
+      end
+
+      it '出発時に現在の駅を取得する' do
+        expect(@walk.current_station.name).to eq '品川'
+      end
+
+      it '複数の駅に到着後、現在の駅を取得する' do
+        create_arrivals(29)
+        expect(@walk.current_station.name).to eq '高輪ゲートウェイ'
+      end
     end
 
-    it '複数の駅に到着済みの時、直近で到着した駅が現在の駅になる' do
-      create_arrivals(29)
-      expect(@walk.current_station.name).to eq '高輪ゲートウェイ'
+    context '内回りモードの時' do
+      before do
+        @walk = FactoryBot.create(:walk, clockwise: false)
+        @arrival = @walk.arrivals.create!(station_id: 1, arrived_at: Time.current)
+      end
+
+      it '出発時に現在の駅を取得する' do
+        expect(@walk.current_station.name).to eq '品川'
+      end
+
+      it '複数の駅に到着後、現在の駅を取得する' do
+        create_arrivals(29)
+        expect(@walk.current_station.name).to eq '大崎'
+      end
     end
   end
 
-  describe '到着駅を取得する' do
-    it 'スタート時には出発駅のみの配列が返る' do
-      expect(@walk.arrived_stations.map(&:id)).to eq([1])
+  describe '到着駅一覧を取得する' do
+    context '外回りモードの時' do
+      before do
+        @walk = FactoryBot.create(:walk)
+        @arrival = @walk.arrivals.create!(station_id: 1, arrived_at: Time.current)
+      end
+
+      it '出発直後に到着駅一覧を取得する' do
+        expect(@walk.arrived_stations.map(&:id)).to eq([1])
+      end
+
+      it '複数の駅に到着後、到着駅一覧を取得する' do
+        create_arrivals(29)
+        expect(@walk.arrived_stations.length).to eq(30)
+      end
     end
 
-    it '複数の駅に到着済みの時、駅の配列が返る' do
-      create_arrivals(29)
-      expect(@walk.arrived_stations.length).to eq(30)
-    end
-  end
+    context '内回りモードの時' do
+      before do
+        @walk = FactoryBot.create(:walk, clockwise: false)
+        @arrival = @walk.arrivals.create!(station_id: 1, arrived_at: Time.current)
+      end
 
-  def create_arrivals(number)
-    (1..number).each do |n|
-      if n == 30
-        @walk.arrivals.create!(station_id: 1, arrived_at: Time.current)
-      else
-        @walk.arrivals.create!(station_id: n + 1, arrived_at: Time.current)
+      it '出発直後に到着駅一覧を取得する' do
+        expect(@walk.arrived_stations.map(&:id)).to eq([1])
+      end
+
+      it '複数の駅に到着後、到着駅一覧を取得する' do
+        create_arrivals(29)
+        expect(@walk.arrived_stations.length).to eq(30)
       end
     end
   end

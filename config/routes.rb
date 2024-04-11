ca2247a4dabeed45a6e3f89b78cd0c1d96811557
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-04-11 15:00:52 +0900

    公開URLのルーティング・viewを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :users do
+    get 'arrivals/index'
+  end
   resource :walk, except: [:index]
   resources :arrivals, except: [:new]
   get '/stations', to: 'stations#index', defaults: { format: 'json' }
+  get '/users/:id/arrivals', to: 'users/arrivals#index', :as => :public_arrivals
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   devise_scope :user do

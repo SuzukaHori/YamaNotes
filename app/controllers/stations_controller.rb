0871b416d956a94d2ef2b12d1dc65051cb370ca7
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-04-12 15:55:40 +0900

    地図のデータ受け渡しをvalueを使う方法に変更

--- app/controllers/stations_controller.rb
+++
@@ -1,8 +0,0 @@
-class StationsController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-    stations = Station.all
-    render json: stations
-  end
-end
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -4,26 +4,36 @@ let map = null; // 地図が複数回初期化されるとエラーが発生す�
 
 // Connects to data-controller="map"
 export default class extends Controller {
-  static targets = ["throughStationIds", "currentId"];
+  static targets = ["currentId"];
+  static values = {
+    stations: Array,
+    arrivedIds: Array,
+    currentId: String,
+  };
 
   /* eslint-disable no-undef */
   connect() {
-    fetch("/stations")
-      .then((response) => response.json())
-      .then((stations) => {
-        this.allStations = stations;
-        this.arrivedStations = this.setArrivedStations();
-        this.setMap();
-      })
-      .then(() => {
+    this.allStations = this.stationsValue;
+    this.arrivedStations = this.setArrivedStations();
+    this.currentStationId = parseInt(this.currentIdValue);
+
+    if (!document.getElementById("map")._leaflet_id) {
+      this.setMap().then(() => {
         this.addLine({ stations: this.allStations, color: "green" });
         this.addPins(this.allStations);
         this.addLine({ stations: this.arrivedStations, color: "red" });
       });
+    }
   }
-  
+
+  setArrivedStations() {
+    return this.arrivedIdsValue.map((id) =>
+      this.allStations.find((station) => station.id === id),
+    );
+  }
+
   setMap() {
-    if (!document.getElementById("map")._leaflet_id) {
+    return new Promise((resolve) => {
       const centerPosition = [35.68032, 139.73946];
       map = L.map("map").setView(centerPosition, 12);
       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -31,31 +41,23 @@ export default class extends Controller {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright/ja">OpenStreetMap</a>contributors',
       }).addTo(map);
-    }
-  }
-
-  setArrivedStations() {
-    const throughStationIds = JSON.parse(this.throughStationIdsTarget.value);
-    return throughStationIds.map((id) =>
-      this.allStations.find((station) => station.id === id),
-    );
+      resolve();
+    });
   }
 
   addPins(stations) {
     const myIcon = L.divIcon({ className: "map-icon" });
-    this.currentStationId = parseInt(this.currentIdTarget.value);
 
     for (let i = 0; i < stations.length; i++) {
       const station = stations[i];
+      const pin = L.marker([station.latitude, station.longitude], {
+        icon: myIcon,
+      })
+        .addTo(map)
+        .bindPopup(`${station.name}駅`);
+
       if (station.id === this.currentStationId) {
-        L.marker([station.latitude, station.longitude], { icon: myIcon })
-          .addTo(map)
-          .bindPopup(`${station.name}駅`)
-          .openPopup();
-      } else {
-        L.marker([station.latitude, station.longitude], { icon: myIcon })
-          .addTo(map)
-          .bindPopup(`${station.name}駅`);
+        pin.openPopup();
       }
     }
   }
@@ -65,9 +67,6 @@ export default class extends Controller {
     for (let i = 0; i < stations.length; i++) {
       const station = stations[i];
       locations.push([station.latitude, station.longitude]);
-    }
-    if (stations.length === this.allStations.length) {
-      locations.push([stations[0].latitude, stations[0].longitude]);
     }
     L.polyline(locations, { color, weight: 15, opacity: 0.4 }).addTo(map);
   }

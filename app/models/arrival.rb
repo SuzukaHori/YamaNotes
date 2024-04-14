739f18933959cfdfb1bf2fbc5924f38b4a7dd0b5
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-04-14 22:19:48 +0900

    編集時に到着時間を不正な値にできないようバリデーションを追加

--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -4,9 +4,20 @@ class Arrival < ApplicationRecord
   validates :arrived_at, presence: true
   validate :prohibit_arrival_without_next_station, if: -> { validation_context == :create }
   before_validation :convert_blank_to_nil
+  validate :check_arrived_time, if: -> { validation_context == :update }
   before_destroy :check_arrival_location, unless: -> { destroyed_by_association }
 
   private
+
+  def check_arrived_time
+    arrivals = walk.arrivals.order(arrived_at: :asc)
+    self_index = arrivals.find_index(self)
+    next_arrival_time = arrivals[self_index + 1]&.arrived_at
+    prev_arrival_time = arrivals[self_index - 1].arrived_at
+    return if (prev_arrival_time..next_arrival_time).cover? arrived_at
+
+    errors.add :arrived_at, '到着時間が不正です'
+  end
 
   def prohibit_arrival_without_next_station
     return if walk.arrivals.empty?
@@ -27,7 +38,6 @@ class Arrival < ApplicationRecord
   end
 
   def check_arrival_location
-    return if destroyed_by_association
     return if self == walk.arrivals.order(:arrived_at).last
 
     errors.add :station_id, '最後の到着以外は削除できません'

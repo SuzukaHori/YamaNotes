fda3a38e390f5b1724f99715ebfcccad6b58de38
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-04-14 15:45:55 +0900

    walk削除時にarrivalのバリデーションをスキップするよう設定

--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -4,7 +4,7 @@ class Arrival < ApplicationRecord
   validates :arrived_at, presence: true
   validate :prohibit_arrival_without_next_station, if: -> { validation_context == :create }
   before_validation :convert_blank_to_nil
-  before_destroy :check_arrival_location
+  before_destroy :check_arrival_location, unless: -> { destroyed_by_association }
 
   private
 
@@ -27,6 +27,7 @@ class Arrival < ApplicationRecord
   end
 
   def check_arrival_location
+    return if destroyed_by_association
     return if self == walk.arrivals.order(:arrived_at).last
 
     errors.add :station_id, '最後の到着以外は削除できません'

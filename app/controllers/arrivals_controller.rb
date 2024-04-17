d4d833c35a19ee88f59c605cad6b5368b1b0945f
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-04-17 15:38:27 +0900

    flashをTurboStream化

--- app/controllers/arrivals_controller.rb
+++ app/controllers/arrivals_controller.rb
@@ -1,7 +1,7 @@
 class ArrivalsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_arrival, only: %i[show edit update destroy]
-  before_action :set_walk, only: %i[index show create]
+  before_action :set_walk, only: %i[index show create update]
 
   def index
     @arrivals = current_walk.sorted_arrivals
@@ -22,8 +22,7 @@ class ArrivalsController < ApplicationController
 
   def update
     if @arrival.update(arrival_params)
-      # redirect_to request.referer, notice: '到着記録を更新しました'
-      @walk = current_walk
+      flash.now.notice = '到着記録を更新しました'
       @arrivals = current_walk.arrivals.order(:id)
     else
       render 'edit', status: :unprocessable_entity

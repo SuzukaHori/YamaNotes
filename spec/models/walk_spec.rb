639c752502b6861e982cf5308113eb6c35b8c2bb
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-04-23 15:12:26 +0900

    walkのモデルテストを修正

--- spec/models/walk_spec.rb
+++ spec/models/walk_spec.rb
@@ -1,70 +1,64 @@
 require 'rails_helper'
 
+shared_context 'clockwise' do
+  let(:walk) { FactoryBot.create(:walk) }
+  let!(:arrival) { FactoryBot.create(:arrival, walk:, station_id: 1) }
+end
+
+shared_context 'counter_clockwise' do
+  let(:walk_counter_clockwise) { FactoryBot.create(:walk, clockwise: false) }
+  let!(:arrival) { FactoryBot.create(:arrival, walk: walk_counter_clockwise, station_id: 1) }
+end
+
 RSpec.describe Walk, type: :model do
-  describe '現在の駅を取得する' do
-    context '外回りモードの時' do
-      before do
-        @walk = FactoryBot.create(:walk)
-        @arrival = @walk.arrivals.create!(station_id: 1, arrived_at: Time.current)
+  describe '#current_station' do
+    context '外回りモードの場合' do
+      include_context 'clockwise'
+      it '出発時に現在の駅を取得できること' do
+        expect(walk.current_station.name).to eq '品川'
       end
 
-      it '出発時に現在の駅を取得する' do
-        expect(@walk.current_station.name).to eq '品川'
-      end
-
-      it '複数の駅に到着後、現在の駅を取得する' do
-        create_arrivals(29)
-        expect(@walk.current_station.name).to eq '高輪ゲートウェイ'
+      it '複数の駅に到着後、現在の駅を取得できること' do
+        create_arrivals(walk, 29)
+        expect(walk.current_station.name).to eq '高輪ゲートウェイ'
       end
     end
 
-    context '内回りモードの時' do
-      before do
-        @walk = FactoryBot.create(:walk, clockwise: false)
-        @arrival = @walk.arrivals.create!(station_id: 1, arrived_at: Time.current)
-      end
-
+    context '内回りモードの場合' do
+      include_context 'counter_clockwise'
       it '出発時に現在の駅を取得する' do
-        expect(@walk.current_station.name).to eq '品川'
+        expect(walk_counter_clockwise.current_station.name).to eq '品川'
       end
 
       it '複数の駅に到着後、現在の駅を取得する' do
-        create_arrivals(29)
-        expect(@walk.current_station.name).to eq '大崎'
+        create_arrivals(walk_counter_clockwise, 29)
+        expect(walk_counter_clockwise.current_station.name).to eq '大崎'
       end
     end
   end
 
-  describe '到着駅一覧を取得する' do
-    context '外回りモードの時' do
-      before do
-        @walk = FactoryBot.create(:walk)
-        @arrival = @walk.arrivals.create!(station_id: 1, arrived_at: Time.current)
+  describe '#arrived_stations' do
+    context '外回りモードの場合' do
+      include_context 'clockwise'
+      it '出発直後に到着駅一覧を取得できること' do
+        expect(walk.arrived_stations.map(&:id)).to eq([1])
       end
 
-      it '出発直後に到着駅一覧を取得する' do
-        expect(@walk.arrived_stations.map(&:id)).to eq([1])
-      end
-
-      it '複数の駅に到着後、到着駅一覧を取得する' do
-        create_arrivals(29)
-        expect(@walk.arrived_stations.length).to eq(30)
+      it '複数の駅に到着後、到着駅一覧を取得できること' do
+        create_arrivals(walk, 29)
+        expect(walk.arrived_stations.length).to eq(30)
       end
     end
 
-    context '内回りモードの時' do
-      before do
-        @walk = FactoryBot.create(:walk, clockwise: false)
-        @arrival = @walk.arrivals.create!(station_id: 1, arrived_at: Time.current)
+    context '内回りモードの場合' do
+      include_context 'counter_clockwise'
+      it '出発直後に到着駅一覧を取得できること' do
+        expect(walk_counter_clockwise.arrived_stations.map(&:id)).to eq([1])
       end
 
-      it '出発直後に到着駅一覧を取得する' do
-        expect(@walk.arrived_stations.map(&:id)).to eq([1])
-      end
-
-      it '複数の駅に到着後、到着駅一覧を取得する' do
-        create_arrivals(29)
-        expect(@walk.arrived_stations.length).to eq(30)
+      it '複数の駅に到着後、到着駅一覧を取得できること' do
+        create_arrivals(walk_counter_clockwise, 29)
+        expect(walk_counter_clockwise.arrived_stations.length).to eq(30)
       end
     end
   end

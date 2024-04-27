9a1f545156294d792e5363f88b8e03fc19928230
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-04-27 21:57:25 +0900

    topページをhigh_voltageに即した書き方に変更

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
     post 'logout', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
-  root to: 'top#index'
   get 'up' => 'rails/health#show', as: :rails_health_check
 end

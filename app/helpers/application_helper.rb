33d81599d684e90b8285203dd6abeb26bb93d13a
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-05-04 18:42:21 +0900

    OGPを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,23 @@ module ApplicationHelper
     {
       site: 'YamaNotes',
       reverse: true,
-      separator: '|'
+      charset: 'utf-8',
+      description: '山手線一周に徒歩で挑戦する人のための記録アプリ',
+      keywords: '山手線一周,山手線,徒歩',
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        image: image_url('ogp.png')
+      }
     }
   end
 end

5ec6f4b1a92219a351018f232bcdbf2d5531e6a2
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-05-09 23:25:35 +0900

    失敗するテストを修正

--- spec/system/users_arrivals_spec.rb
+++ spec/system/users_arrivals_spec.rb
@@ -15,10 +15,10 @@ RSpec.describe 'Users/Arrivals', type: :system do
     it '公開用URLをコピーする' do
       sign_in user
       setting_and_visit_public_path(walk_public)
-      url = find_field('公開用URL:').value
+      url = find_field('URL').value
       expect(url).to include(user_arrivals_path(user))
       click_on 'copy_button'
-      expect(page).to have_content('Copied')
+      expect(page.accept_confirm).to eq '共有用URLをクリップボードにコピーしました'
     end
   end
 
@@ -26,7 +26,8 @@ RSpec.describe 'Users/Arrivals', type: :system do
     it '公開状態の到着履歴にアクセスする' do
       setting_and_visit_public_path(walk_public)
       expect(page).to_not have_content('編集')
-      expect(page).to_not have_content('公開用URL:')
+      expect(page).to_not have_content('到着を削除')
+      expect(page).to_not have_content('URL')
     end
 
     it '未公開状態の到着履歴にアクセスできない' do

cf7875fd746b747f4925b6330508d18b2a767720
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-05-19 11:47:43 +0900

    デバッグ用のコードを削除

--- app/javascript/controllers/time_controller.js
+++ app/javascript/controllers/time_controller.js
@@ -4,15 +4,12 @@ export default class extends Controller {
   static values = { departureDate: String };
 
   connect() {
-    console.log('time')
     const departureDate = new Date(this.departureDateValue);
     setInterval(() => this._displayTime(departureDate), 1000);
   }
 
   _displayTime(departureDate) {
-    console.log('time2')
     const element = document.querySelector(".time");
-    console.log(element)
     if (element) {
       const now = new Date();
       const elapsedTime = this._localizeTime(now - departureDate);
@@ -21,7 +18,6 @@ export default class extends Controller {
   }
 
   _localizeTime(time) {
-    console.log('time')
     const hours = Math.floor(time / (1000 * 60 * 60));
     const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
     return `${hours}時間${minutes}分`;

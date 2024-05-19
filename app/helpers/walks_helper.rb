0f7afefa421ad7faacab305dbe2fceacfe9d0f8b
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-05-19 10:56:52 +0900

    arrivalsのヘルパーのテストを追加

--- app/helpers/walks_helper.rb
+++ app/helpers/walks_helper.rb
@@ -5,14 +5,20 @@ module WalksHelper
 
   def elapsed_time(walk)
     elapsed_seconds = Time.current - walk.created_at
-    hours = (elapsed_seconds / 3600).to_i
-    minutes = ((elapsed_seconds % 3600) / 60).to_i
-    "#{hours}時間#{minutes}分"
+    convert_to_local_time(elapsed_seconds)
   end
 
   def time_to_reach_goal(walk)
     return unless walk.finished?
 
-    Time.at(walk.arrival_of_goal.arrived_at - walk.arrival_of_departure.arrived_at).utc.strftime('%k時間%M分')
+    convert_to_local_time(walk.arrival_of_goal.arrived_at - walk.created_at)
+  end
+
+  private
+
+  def convert_to_local_time(m_seconds)
+    hours = (m_seconds / 3600).to_i
+    minutes = ((m_seconds - (hours * 3600)) / 60).to_i
+    "#{hours}時間#{minutes.to_i}分"
   end
 end

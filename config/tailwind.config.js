4d651cf276dc7427d52126161b6f50ca34161006
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-05-25 16:06:26 +0900

    トップページのデザインを修正

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,7 +11,19 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        pop: ['Mochiy Pop One', 'sans-serif'],
         meiryo: ['メイリオ', 'Meiryo','Hiragino Kaku Gothic ProN','ヒラギノ角ゴ ProN W3','ＭＳ Ｐゴシック','MS PGothic', 'MS UI Gothic','Helvetica','Arial']
+      },
+      colors: {
+        'yamanote-green': {
+          50: '#D8DED2',
+          100: '#6F9142',
+        },
+        'bright-orange': {
+          50: '#f2bf91',
+          100: '#FD5B40',
+          200: '#cc6206'
+        }
       },
     },
   },

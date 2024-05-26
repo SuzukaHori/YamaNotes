0a9972e3f39f12298560fbc66f4d1492b6f8b4bf
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-05-26 21:49:45 +0900

    本番環境で画像が切り替わらないバグを修正

--- app/javascript/controllers/clockwise_controller.js
+++ app/javascript/controllers/clockwise_controller.js
@@ -2,12 +2,20 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["guide"];
+  static values = { truePath: String, falsePath: String };
 
-  true() {
-    this.guideTarget.src = "/assets/clockwise.png";
+  async connect() {
+    const trueImage = await fetch(this.truePathValue);
+    this.truePath = trueImage.url;
+    const falseImage = await fetch(this.falsePathValue);
+    this.falsePath = falseImage.url;
+  }
+
+  async true() {
+    this.guideTarget.src = this.truePath;
   }
 
   false() {
-    this.guideTarget.src = "/assets/counter_clockwise.png";
+    this.guideTarget.src = this.falsePath;
   }
 }

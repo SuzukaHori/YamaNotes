bdae53d44b8fc4e3c5b0ed15dd3f3f9c4bbfcbc7
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-06-09 19:49:44 +0900

    マイグレーションファイルを分割

--- db/migrate/20240319011328_devise_create_users.rb
+++ db/migrate/20240319011328_devise_create_users.rb
@@ -6,7 +6,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       t.numeric :uid, null: false
       t.string :provider, null: false
       t.timestamps null: false
-      t.timestamp :remember_created_at
     end
     add_index :users, [:uid, :provider],  unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_23_055616) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_09_104109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 

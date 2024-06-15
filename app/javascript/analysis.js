42bab6d4bcaa98314bcb622d0d9d154ea2857634
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-06-15 20:58:29 +0900

    リンターの指摘を修正

--- app/javascript/analysis.js
+++ app/javascript/analysis.js
@@ -1,5 +1,7 @@
 window.dataLayer = window.dataLayer || [];
-// eslint-disable-next-line no-undef
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-VQT4R8C8MZ');
+function gtag() {
+  // eslint-disable-next-line no-undef
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-VQT4R8C8MZ");

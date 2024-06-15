5ce16a2c906261cebaae2215fe0a38ee8e283d72
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-06-15 20:55:36 +0900

    Googleアナリティクスのタグを埋め込み

---
+++ app/javascript/analysis.js
@@ -0,0 +1,5 @@
+window.dataLayer = window.dataLayer || [];
+// eslint-disable-next-line no-undef
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-VQT4R8C8MZ');

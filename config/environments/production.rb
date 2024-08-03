2eea447f32789994da4dcfa99b75b54610430b14
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-08-03 23:50:09 +0900

    セッションの保存先の設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,8 +31,6 @@ Rails.application.configure do
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  # config.session_store :cache_store, expire_after: 3.days
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 

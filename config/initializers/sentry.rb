fd5d1f4955a39e29293cb07919e8d75f6293144d
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-08-31 20:17:29 +0900

    開発環境のエラーを飛ばさないようSentryの設定を変更

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = 'https://b7871da3f2f0d9207bbd39e0bdc8640a@o4507770301251584.ingest.us.sentry.io/4507770301513728'
+  config.dsn = ENV['RAILS_ENV'] == 'production' ? ENV["SENTRY_KEY"] : ''
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%

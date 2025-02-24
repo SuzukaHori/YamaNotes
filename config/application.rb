856b3f452a98d3a6219c5c976a31742ed00fc5bc
SuzukaHori <horikoshi.suzuka@gmail.com>
2025-02-24 13:29:43 +0900

    警告が出ていたため、タイムゾーンの設定を修正

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module Yamanotes
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = 'Tokyo'
+    config.active_support.to_time_preserves_timezone = :zone
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")

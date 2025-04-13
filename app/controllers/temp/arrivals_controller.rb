839ee9689c6378c9bd70dc4d677ad57c6522c322
SuzukaHori <horikoshi.suzuka@gmail.com>
2025-04-13 21:28:10 +0900

    URLが適切にリダイレクトするよう修正

---
+++ app/controllers/temp/arrivals_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Temp::ArrivalsController < ApplicationController
+  skip_before_action :authenticate_user!
+  # URL変更のため一時的に定義しているが、後に削除する
+
+  def index
+    user = User.find(params[:user_id])
+    walk = user.walks.order(:created_at).first!
+    redirect_to walk_arrivals_url(walk)
+  end
+end

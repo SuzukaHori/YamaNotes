0bbdcd1234075fd1ddd4faf9d1f25983fe1c389a
SuzukaHori <horikoshi.suzuka@gmail.com>
2025-04-13 20:42:15 +0900

    current_walkメソッドを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  helper_method :current_walk
 
   def current_walk
-    current_user&.walk
+    # TODO: テストを直す
+    return nil if current_user.walks.empty?
+
+    current_user.walks.order(:created_at).last
   end
 end

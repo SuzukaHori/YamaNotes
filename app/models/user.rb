28dbd4fc67316f769a282d84bd990a3ade851a78
SuzukaHori <horikoshi.suzuka@gmail.com>
2025-04-13 20:22:00 +0900

    userとwalkの関連付けをhas_manyにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 
 class User < ApplicationRecord
   devise :omniauthable, :rememberable, omniauth_providers: %i[google_oauth2]
-  has_one :walk, dependent: :destroy
+  has_many :walks, dependent: :destroy
   has_many :arrivals, through: :walk
   validates :uid, uniqueness: { scope: :provider }, numericality: { only_integer: true }, presence: true
   validates :provider, inclusion: { in: ['google_oauth2'] }, presence: true

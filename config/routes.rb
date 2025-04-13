3b6a25af67a51d961d1bfe8ad43d239e51cb3853
SuzukaHori <horikoshi.suzuka@gmail.com>
2025-04-13 21:09:49 +0900

    公開ページのURLを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :walks
+  resources :walks do
+    resources :arrivals, only: [:index], :to => 'walks/arrivals#index'
+  end
   resources :arrivals, except: [:new] do
     resource :report, only: %i(show), controller: "arrivals/report"
   end
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :users, only: [:destroy] do
-    resources :arrivals, only: [:index], :to => 'users/arrivals#index'
-  end
+  # resources :users, only: [:destroy] do
+  #   resources :arrivals, only: [:index], :to => 'users/arrivals#index'
+  # end
+  resources :users, only: [:destroy]
   devise_scope :user do
     post 'logout', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end

02c8d37a48818f3b3673aaffffc01bdffd40a784
SuzukaHori <horikoshi.suzuka@gmail.com>
2025-05-11 22:53:55 +0900

    walkとuserのファクトリにtraitを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,11 @@ FactoryBot.define do
   factory :user do
     sequence(:uid) { |n| 10_000_000_000 + n }
     provider { 'google_oauth2' }
+
+    trait :with_walk do
+      after(:create) do |user|
+        FactoryBot.create(:walk, :with_arrivals, user: user)
+      end
+    end
   end
 end

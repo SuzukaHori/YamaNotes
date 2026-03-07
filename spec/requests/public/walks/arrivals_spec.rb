567197a142e2752fd08ec6f1146c2a68c55755b2
SuzukaHori <horikoshi.suzuka@gmail.com>
2026-03-07 21:58:18 +0900

    冗長なテストを修正

--- spec/requests/public/walks/arrivals_spec.rb
+++ spec/requests/public/walks/arrivals_spec.rb
@@ -4,10 +4,10 @@ require 'rails_helper'
 
 RSpec.describe 'Public::Walks::Arrivals', type: :request do
   describe 'GET /public/walks/:walk_id/arrivals' do
-    subject(:get_public_walk_arrivals) { get public_walk_arrivals_path(walk) }
+    subject(:get_public_walk_arrivals) { get public_walk_arrivals_path(walk_id: walk_id) }
 
     context '公開されている歩行記録の場合' do
-      let!(:walk) { FactoryBot.create(:walk, publish: true) }
+      let(:walk_id) { FactoryBot.create(:walk, publish: true).id }
 
       it '200が返る' do
         get_public_walk_arrivals
@@ -16,7 +16,7 @@ RSpec.describe 'Public::Walks::Arrivals', type: :request do
     end
 
     context '非公開の歩行記録の場合' do
-      let!(:walk) { FactoryBot.create(:walk, publish: false) }
+      let(:walk_id) { FactoryBot.create(:walk, publish: false).id }
 
       it 'rootにリダイレクトされる' do
         get_public_walk_arrivals
@@ -25,7 +25,7 @@ RSpec.describe 'Public::Walks::Arrivals', type: :request do
     end
 
     context '存在しない歩行記録の場合' do
-      let(:walk) { instance_double('Walk', to_param: '0') }
+      let(:walk_id) { 0 }
 
       it '404が返る' do
         get_public_walk_arrivals

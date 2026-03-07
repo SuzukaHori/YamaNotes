651c0fa375c2c3fba20d1dfe95ba0a2a93fe75cb
SuzukaHori <horikoshi.suzuka@gmail.com>
2026-03-07 21:52:50 +0900

    到達しないコードを削除

--- app/controllers/public/walks/arrivals_controller.rb
+++ app/controllers/public/walks/arrivals_controller.rb
@@ -8,7 +8,6 @@ class Public::Walks::ArrivalsController < ApplicationController
     if @walk.publish
       @arrivals = @walk.arrivals.order(:created_at).includes(:station)
       @user = current_user
-      render 'arrivals/index'
     else
       redirect_to root_path, notice: 'この到着記録は非公開です'
     end
@@ -18,9 +17,5 @@ class Public::Walks::ArrivalsController < ApplicationController
 
   def set_walk
     @walk = Walk.find(params[:walk_id])
-
-    return if @walk
-
-    redirect_to root_path, alert: '歩行記録が存在しません。'
   end
 end
